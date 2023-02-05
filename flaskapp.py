f15256c1e8c911e5027d93e67cd8b7051e28acdb
neerajmenon <neeraj.menon.m@gmail.com>
2023-02-05 11:56:26 -0500

    Added basic login verification and dashboard

--- flaskapp.py
+++ flaskapp.py
@@ -13,8 +13,41 @@ def hello_world():
 
 @app.route('/login',methods=['GET','POST'])
 def login():
-    return render_template('login.html')
+  if request.method == 'POST':
+        connection = sqlite3.connect('user_data.db')
+        cursor = connection.cursor()
 
+        name = request.form['username']
+        password = request.form['password']
+        command = "SELECT DISTINCT * from users where name='"+name+"';"
+        print(command)
+        cursor.execute(command)
+        rows = cursor.fetchall()
+        f_name = ""
+        f_pswd = ""
+        f_email = ""
+        for row in rows:
+          print(row)
+        if len(rows)>0:
+          print(len(rows[0]))
+          f_name = rows[0][0]
+          f_pswd = rows[0][1]
+          f_email = rows[0][2]
+          f_fname = rows[0][3]
+          f_lname = rows[0][4]
+        if f_name == name:
+          if f_pswd == password:
+            flash('You were successfully logged in!')
+            return render_template('dashboard.html',username=f_name,password=f_pswd,email=f_email,fname=f_fname,lname=f_lname)
+          else:
+            flash("Incorrect password")
+        else:
+          flash("Username does not exist")
+          
+        
+        return render_template('login.html')
+  elif request.method == 'GET':
+    return render_template('login.html')
 
 @app.route('/register',methods=['GET','POST'])
 def register():
@@ -32,6 +65,7 @@ def register():
         print(command)
         cursor.execute(command)
         connection.commit()
+        return render_template('login.html')
 
     return render_template('register.html')
 
